05b6de12256c61a4e798a81eaa65d4981a477c99
Fatawo <104683683+Fatawo@users.noreply.github.com>
2023-09-12 13:05:31 +0000

    Add files via upload

---
+++ MINI PROJECT/js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Choose the correct HTML element to define important text.",
+    choice1: "<strong>",
+    choice2: "<i>",
+    choice3: "<important>",
+    choice4: "<b>",
+    answer: 0
+},
+            {
+            question: "Which HTML tag is used to define an internal style sheet?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 1
+},
+            {
+            question: "How do you make a list that list its items with square?",
+    choice1: "list-style-type:square",
+    choice2: "list:square",
+    choice3: "list-type:square",
+    choice4: "list=style:square",
+    answer: 0
+},
+            {
+            question: "What is the default value of the position property?",
+    choice1: "absolute",
+    choice2: "static",
+    choice3: "fixed",
+    choice4: "relative",
+    answer: 1
+},
+            {
+            question: "How do you declare a JavaScript variable?",
+    choice1: "var carName",
+    choice2: "v carName",
+    choice3: "varible carName",
+    choice4: "variable car",
+    answer: 0
+},
+            {
+            question: "What is the correct way to create a function in pyhon?",
+    choice1: "function myfunction()",
+    choice2: "def myFunction()",
+    choice3: "myfunction",
+    choice4: "create myFunction",
+    answer: 1
+},
+            {
+               question: "Which collection does not allow duplicate members?",
+    choice1: "LIST",
+    choice2: "TUPLE",
+    choice3: "DICTIONARY",
+    choice4: "SET",
+    answer: 3
+},{
+               question: "Which statement is used to stop loop?",
+    choice1: "exit",
+    choice2: "stop",
+    choice3: "break",
+    choice4: "return",
+    answer: 2
+},
+            {
+               question: "Which SQL statement is used to extract data from a database?",
+    choice1: "GET",
+    choice2: "SELECT",
+    choice3: "EXTRACT",
+    choice4: "OPEN",
+    answer: 1
+},
+            {
+               question: "How do you call a function in C++?",
+    choice1: "functionName;",
+    choice2: "functionName[];",
+    choice3: "(functionName)",
+    choice4: "functionName()",
+    answer: 3
+}];
